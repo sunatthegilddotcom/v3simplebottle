0bc7d551f5f3300672dea7bb16a937e0a9111fc3
sunatthegilddotcom <sun@thegild.com>
2016-02-25 22:40:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bottle import route, run
 @route('/')
 def index():
     return "<h1> Hello Corelogic OpenShift</h1>"
+    return "<h2> CI/CD with agile development</h2>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
